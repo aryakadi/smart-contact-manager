926e67ed473716de16b418f8e3aea5fc66cd7ce6
aryakadi <aryakadi2412@gmail.com>
2026-04-10 12:22:05 +0530

    correction

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,10 +2,10 @@ import api from './api'
 
 const authService = {
   register: (name, email, password) =>
-    api.post('/auth/register', { name, email, password }).then(r => r.data),
+    api.post('api/auth/register', { name, email, password }).then(r => r.data),
 
   login: (email, password) =>
-    api.post('/auth/login', { email, password }).then(r => r.data),
+    api.post('api/auth/login', { email, password }).then(r => r.data),
 }
 
 export default authService
